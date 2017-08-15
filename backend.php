cd64a8a6e78d9f235020ddef40e63e903c860adb
gwcteam17 <gwcteam17@gmail.com>
2017-08-15 15:12:58 -0700

    fixed formatting issues

--- backend.php
+++ backend.php
@@ -8,5 +8,8 @@ if (empty($_GET["url"])) {
   if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
     echo "Invalid URL";
 }
+  else {
+    echo "Valid"
+  }
 }
 ?>
